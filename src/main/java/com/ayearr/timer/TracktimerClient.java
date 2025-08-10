be69e6ff7d10959365bb289c3bff63c1b61dcbd3
Ayearr <iamrybo@outlook.com>
2025-08-10 09:22:35 +0800

    qwq

---
+++ src/main/java/com/ayearr/timer/TracktimerClient.java
@@ -0,0 +1,10 @@
+package com.ayearr.timer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TracktimerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/com/ayearr/timer/TracktimerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ayearr.timer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TracktimerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
